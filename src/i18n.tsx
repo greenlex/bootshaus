41c58db8f42261da6072360bea45c804993a62a6
algruenw <alexander.gruenwald2@dhl.com>
2023-09-17 17:34:05 +0200

    Update translation

--- src/i18n.tsx
+++ src/i18n.tsx
@@ -3,12 +3,24 @@ import { initReactI18next } from 'react-i18next';
 
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import translationEN from "./locales/en/translation.json";
+import translationDE from "./locales/de/translation.json";
+
+const resources = {
+  en: {
+    translation: translationEN
+  },
+  de: {
+    translation: translationDE
+  }
+};
 
 i18n
     .use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
+      resources,
       fallbackLng: 'de',
       debug: true,
 
